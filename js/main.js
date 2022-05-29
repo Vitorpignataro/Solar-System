3074c56d0090bb4e99dd8f6ea8b905868339e2da
José Vitor <josevitorpignataro@gmail.com>
2022-05-29 19:56:18 -0300

    FEAT: site structure done, effects td

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*
     Controles da navegação
+    Sistema para deixar o conteúdo da navigação selecionado ativado
 */
 let linkNav = document.getElementsByClassName("nav-item");
 
@@ -10,3 +11,27 @@ for(let i = 0; i < linkNav.length; i++){
         this.className += " menu_active"
     })
 }
+
+
+// Função que exibe o conteúdo about ao clicar no botão
+function showContentAbout(){
+    let displayContent2 = document.getElementsByClassName('content-p');
+
+    if(displayContent2[0].style.display == 'none'){
+        displayContent2[0].style.display = 'block';
+        displayContent2[1].style.display = 'none';
+    }    
+}
+
+// Função que exibe o conteúdo curiosidades ao clicar no botão
+function showContentCuri(){
+    let displayContent2 = document.getElementsByClassName('content-p');
+    if(displayContent2[1].style.display == 'none'){
+        displayContent2[1].style.display = 'block';
+        displayContent2[0].style.display = 'none';
+    }
+    
+}
+
+
+
