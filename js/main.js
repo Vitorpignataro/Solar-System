58d601471135520e432c572200589b42a7640994
José Vitor <josevitorpignataro@gmail.com>
2022-05-29 21:27:14 -0300

    FEAT: Background stars complete, td planet 3d

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /*
+    Adicionar Jquery posteriormente
     Controles da navegação
     Sistema para deixar o conteúdo da navigação selecionado ativado
 */
@@ -7,10 +8,26 @@ let linkNav = document.getElementsByClassName("nav-item");
 for(let i = 0; i < linkNav.length; i++){
     linkNav[i].addEventListener('click', function(){
         let CurrentlyClicked = document.getElementsByClassName("menu_active");
-        CurrentlyClicked[0].className = CurrentlyClicked[0].className.replace(" menu_active");
-        this.className += " menu_active"
+        CurrentlyClicked[0].className = CurrentlyClicked[0].className.replace("menu_active");
+        this.className = "nav-item menu_active"
     })
 }
+
+
+/*
+    Controles da navegação
+    Sistema para deixar o conteúdo da navigação selecionado ativado
+*/
+let btn_content = document.getElementsByClassName("btn-content");
+
+for(let i = 0; i < btn_content.length; i++){
+    btn_content[i].addEventListener('click', function(){
+        let CurrentlyClicked = document.getElementsByClassName("content_active");
+        CurrentlyClicked[0].className = CurrentlyClicked[0].className.replace("content_active");
+        this.className = "btn-content content_active"
+    })
+}
+
 
 
 // Função que exibe o conteúdo about ao clicar no botão
